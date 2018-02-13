458f6d3c0485c41f0ec523b14388d456ae7cbb31
Ciara Smith <117381516@umail.ucc.ie>
2018-02-13 12:17:35 +0000

    ff

--- workspace/ebusiness/cost_calc.js
+++ workspace/ebusiness/cost_calc.js
@@ -9,15 +9,15 @@ function calcSub()
     {
         argSubTotal = 100; //If salesforce is checked the variable = 100
     }
-   else if (document.getElementById('aws').checked)
+    if (document.getElementById('aws').checked)
     {
         argSubTotal = 300; //If Amazon Web Service is checked the variable = 300
     }
-   else if (document.getElementById('Cloud 9').checked)
+   if (document.getElementById('Cloud 9').checked)
     {
         argSubTotal = 200; //If Cloud 9 is checked the variable = 200
     }
-    else (document.getElementById('Gmail').checked)
+    if (document.getElementById('Gmail').checked)
     {
         argSubTotal = 400; //If Gmail is checked the variable = 400
     }
