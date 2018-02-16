0a59bfecf123cf355b7881f39206ca9fbbb0ad3d
Ciara Smith <117381516@umail.ucc.ie>
2018-02-16 11:23:35 +0000

    button

--- workspace/ebusiness/cost_calc.js
+++ workspace/ebusiness/cost_calc.js
@@ -13,15 +13,15 @@ function calcSub()
     {
         argSubTotal = 100; //If salesforce is checked the variable = 100
     }
-    if (document.getElementById('aws').checked)
+   else if (document.getElementById('aws').checked)
     {
         argSubTotal = 300; //If Amazon Web Service is checked the variable = 300
     }
-   if (document.getElementById('Cloud 9').checked)
+  else if (document.getElementById('Cloud 9').checked)
     {
         argSubTotal = 200; //If Cloud 9 is checked the variable = 200
     }
-    if (document.getElementById('Gmail').checked)
+   else (document.getElementById('Gmail').checked)
     {
         argSubTotal = 400; //If Gmail is checked the variable = 400
     }
@@ -30,15 +30,12 @@ function calcSub()
 }
 
 function calcDisVatTotal()
-{
-$x=$_POST['argSubTotal'];
+
 
 {
 discountAmount = argSubTotal * 05;
 }
-display(discountAmount); 
 
-}
 
 
 
@@ -51,12 +48,6 @@ function display(parm1) //parm1 equals value of argSubTotal
     document.getElementById("total").value = parm1;
     
     enablebtnProceed();
-}
-
-function display(parm2)
-{
- document.getElementById("discount").value = parm2;
- enablebtnProceed();
 }
 
 
